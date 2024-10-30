f64add9ef311906036e0c04fab12a49b66a64d33
jbaptiste.go@gmail.com <jbaptiste.go@gmail.com>
2024-10-30 10:34:08 +0000

    update

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,10 +1,12 @@
-Select
-    orders_id,
-    date_date,
-    revenue,
-    quantity,
-    ROUND((purchase_price*quantity),2) AS purchase_cost,
-    ROUND((revenue-(purchase_price*quantity)),2) AS margin
-FROM {{ref("stg_raw__sales")}}
-JOIN {{ref("stg_raw__product")}}
-    USING (products_id)
+  SELECT
+      products_id,
+      date_date,
+      orders_id,
+      revenue,
+      quantity,
+      purchase_price,
+      ROUND(s.quantity*p.purchase_price,2) AS purchase_cost,
+      ROUND(s.revenue - s.quantity*p.purchase_price, 2) AS margin
+  FROM {{ref("stg_raw__sales")}} s
+  LEFT JOIN {{ref("stg_raw__product")}} p
+      USING (products_id)
